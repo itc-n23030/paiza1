f9ef86b17a8feae80e17d7c3f5b14462e77b9c02
yuta miyahira <n23030@std.it-college.ac.jp>
2023-06-28 06:51:30 +0000

    README.md

---
+++ code/long.py
@@ -0,0 +1,5 @@
+def f(N):
+    return "lucky" if N % 7 == 0 else "unlucky"
+
+
+print(f(int(input())))
---
+++ code/short.py
@@ -0,0 +1,5 @@
+def f(N, S):
+    return "\n".join([S] * N)
+
+
+print(f(int(input()), str(input())))
